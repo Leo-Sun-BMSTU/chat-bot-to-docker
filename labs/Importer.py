eea49a53e638a1604769c157351d4cab7cac088b
g-ordey <olenka9818@yandex.ru>
2021-05-23 20:41:39 +0300

    )

---
+++ labs/Importer.py
@@ -0,0 +1,40 @@
+import pickle
+
+import pandas as pd
+
+
+class Importer:
+    data = property()
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        self._data = value
+
+    @data.deleter
+    def data(self):
+        del self._data
+
+    def getFile(self, path):
+        try:
+            self._data = pd.read_csv(path, sep=',')
+            print("File {} is dataframe".format(path))
+            return self
+        except pd.errors.ParserError:
+            print("File {} is not dataframe".format(path))
+        except FileNotFoundError:
+            print("Incorrect file name")
+        try:
+            with open(path, 'rb') as f1:
+                self._data = pickle.load(f1, encoding="UTF-8")
+                print("File {} is pickle".format(path))
+                return self
+        except ValueError:
+            print("File {} is not pickle".format(path))
+        except FileNotFoundError:
+            print("Incorrect file name")
+        except pickle.UnpicklingError:
+            print("File {} is not pickle".format(path))
