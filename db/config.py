f3dc835899994434da004e80650b1fc95164075b
компьютер <wowp.samolet@gmail.com>
2021-08-05 17:21:25 +0300

    ready project

--- db/config.py
+++ db/config.py
@@ -9,7 +9,7 @@ admin_id = int(os.getenv("ADMIN_ID"))
 db_user = os.getenv("DB_USER")
 db_pass = os.getenv("DB_PASS")
 host = "localhost"
-port = 1111
+port = 5432
 
 I18N_DOMAIN = 'testbot'
 BASE_DIR = Path(__file__).parent
