e9fc0bb5c3e5ba6da92b0cbd330ffa22994e2e02
Dmitry Shandrovsky <36291378+Kladium@users.noreply.github.com>
2021-08-05 15:24:30 +0300

    Update config.py

--- db/config.py
+++ db/config.py
@@ -9,7 +9,3 @@ admin_id = int(os.getenv("ADMIN_ID"))
 db_user = os.getenv("DB_USER")
 db_pass = os.getenv("DB_PASS")
 host = "localhost"
-
-I18N_DOMAIN = 'testbot'
-BASE_DIR = Path(__file__).parent
-LOCALES_DIR = BASE_DIR / 'locales'
